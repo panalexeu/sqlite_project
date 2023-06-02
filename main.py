de15710f1f250996193db43b5696be08a8f68609
allexeu <oleksahorch@gmail.com>
2023-06-02 21:10:34 +0300

    screenshots added, db recreated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from database import Database
 
+# run main.py to initialize db, then work with app through terminal calling cli.py file
 if __name__ == "__main__":
     database = Database()
     database.initialize()
